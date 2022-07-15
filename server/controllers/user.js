3b77019ea3e5c5b84874bd5709943fc8503a1c0d
LM-01 <mollinedo.lester@gmail.com>
2022-07-15 17:40:58 -0400

    Added video routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -7,9 +7,12 @@ export const test = (req, res) => {
 export const update = async (req, res, next) => {
     if (req.params.id === req.user.id) {
         try {
-            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-                $set: req.body
-            });
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.id, 
+            {
+                $set: req.body,
+            },{new: true}
+            );
             res.status(200).json(updatedUser);
         } catch (error) {
             next(error);
@@ -19,26 +22,64 @@ export const update = async (req, res, next) => {
     }
 }
 
-export const deleteUser = (req, res, next) => {
-    res.json("Its Successful");
+export const deleteUser = async (req, res, next) => {
+    if (req.params.id === req.user.id) {
+        try {
+            await User.findByIdAndDelete(
+                req.params.id, 
+            );
+            res.status(200).json("User has been deleted");
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        next(createError(403, 'You can only delete your account'));
+    }
 }
 
-export const getUser = (req, res, next) => {
-    res.json("Its Successful");
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
 }
 
-export const subscribe = (req, res, next) => {
-    res.json("Its Successful");
+export const subscribe = async (req, res, next) => {
+    try {
+        await User.findById(req.user.id, {
+            $push: { subscribedUsers: req.params.id },
+        });
+
+        await User.findByIdAndUpdate(req.params.id, {
+            $inc: { subscribers: 1 },
+        });
+        res.status(200).json("Subscription successful");
+    } catch (error) {
+        next(error);
+    }
 }
 
-export const unsubscribe = (req, res, next) => {
-    res.json("Its Successful");
+export const unsubscribe = async (req, res, next) => {
+    try {
+        await User.findById(req.user.id, {
+            $pull: { subscribedUsers: req.params.id },
+        });
+
+        await User.findByIdAndUpdate(req.params.id, {
+            $inc: { subscribers: -1 },
+        });
+        res.status(200).json("Unsubscription successful");
+    } catch (error) {
+        next(error);
+    }
 }
  
-export const like = (req, res, next) => {
+export const like = async (req, res, next) => {
     res.json("Its Successful");
 }
 
-export const dislike = (req, res, next) => {
+export const dislike = async (req, res, next) => {
     res.json("Its Successful");
 }
--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,3 +1,105 @@
+import { createError } from "../error";
+import User from "../models/User";
+import Video from "../models/Video";
+
 export const test = (req, res) => {
     res.json("Its Successful");
+}
+
+export const addVideo = async (req, res, next) => {
+    const newVideo = new Video({userId: req.user.id, ...req.body});
+    try {
+        const savedVideo = await newVideo.save();
+        res.status(200).json(savedVideo);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateVideo = async (req, res, next) => {
+    try {
+        const video = await Video.findById(req.params.id);
+        if (!video) return next(createError(404, "Video not found"));
+        if (req.user.id === video.userId){
+            const updateVideo = await Video.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            },{new: true});
+        } else {
+            return next(createError(403, "You can only update your own video"));
+        }
+        res.status(200).json(updateVideo);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const deleteVideo = async (req, res, next) => {
+    try {
+        const video = await Video.findById(req.params.id);
+        if (!video) return next(createError(404, "Video not found"));
+        if (req.user.id === video.userId){
+            await Video.findByIdAndDelete(
+                req.params.id
+            );
+        } else {
+            return next(createError(403, "You can only update your own video"));
+        }
+        res.status(200).json("Video has been deleted");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getVideo = async (req, res, next) => {
+    try {
+        const video = await Video.findById(req.params.id);
+        if (!video) return next(createError(404, "Video not found"));
+        res.status(200).json(video);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const addView = async (req, res, next) => {
+    try {
+        const video = await Video.findByIdAndUpdate(req.params.id,
+            {$inc: {views: 1}},);
+        if (!video) return next(createError(404, "Video not found"));
+        res.status(200).json("View added");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const random = async (req, res, next) => {
+    try {
+        const videos = await Video.aggregate([{ $sample: { size: 40 } }]);
+        res.status(200).json(videos);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const trend = async (req, res, next) => {
+    try {
+        const videos = await Video.find({}).sort({views: -1}).limit(40);
+        res.status(200).json(videos);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const sub = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.id);
+        const subscribedChannels = user.subscribedUsers;
+
+        const list = Promise.all(
+            subscribedChannels.map(channelId => {
+                return Video.find({userId: channelId});
+            })
+
+    } catch (error) {
+        next(error);
+    }
 }
